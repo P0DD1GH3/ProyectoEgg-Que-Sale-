598143f4e0945dfd10e611755ba656d4ad3c18bb
“Camila <camilaguz54@gmail.com>
2022-04-22 12:45:47 -0300

    initial commit

---
+++ src/test/java/com/fanzin/FanzinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanzin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanzinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
