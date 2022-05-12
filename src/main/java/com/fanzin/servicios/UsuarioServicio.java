b1f0fae1e14e91f7eb301265921a65b5fc805f5c
“Lisandro <gutierrezbagginilisandro99@gmail.com>
2022-05-11 21:07:15 -0300

    Usuario, UsuarioServicio, UsuarioControlador

--- src/main/java/com/fanzin/servicios/UsuarioServicio.java
+++ src/main/java/com/fanzin/servicios/UsuarioServicio.java
@@ -2,6 +2,7 @@ package com.fanzin.servicios;
 
 import com.fanzin.entidades.Imagen;
 import com.fanzin.entidades.Usuario;
+import com.fanzin.enumeraciones.Rol;
 import com.fanzin.repositorios.UsuarioRepositorio;
 import java.util.List;
 import java.util.Optional;
@@ -12,34 +13,35 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class UsuarioServicio {
-
+    
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
-
+    
     @Autowired
     private ImagenServicio imagenServicio;
-
+    
     @Transactional(rollbackFor = Exception.class)
-    public Usuario crear(String nombre, String mail, String contraseña, MultipartFile archivo) throws Exception {
-
-        validar(nombre, mail, contraseña);
-
+    public Usuario crear(String nombre, String mail, String contrasenia, String contrasenia1, MultipartFile archivo) throws Exception {
+        
+        validar(nombre, mail, contrasenia, contrasenia1);
+        
         Usuario usuario = new Usuario();
         usuario.setNombre(nombre);
         usuario.setMail(mail);
-        usuario.setContraseña(contraseña);
+        usuario.setContrasenia(contrasenia);
+        usuario.setRol(Rol.USUARIO);
         Imagen imagen = imagenServicio.guardar(archivo);
         usuario.setImagen(imagen);
-
+        
         return usuarioRepositorio.save(usuario);
-
+        
     }
-
+    
     @Transactional(readOnly = true)
     public Usuario buscarPorId(String id) throws Exception {
-
+        
         Optional<Usuario> respuesta = usuarioRepositorio.findById(id);
-
+        
         if (respuesta.isPresent()) {
             Usuario usuario = respuesta.get();
             return usuario;
@@ -47,52 +49,60 @@ public class UsuarioServicio {
             throw new Exception("No se encontro el evento indicado");
         }
     }
-
+    
     @Transactional(rollbackFor = Exception.class)
-    public Usuario modificar(String id, String nombre, String mail, String contraseña, MultipartFile archivo) throws Exception {
-
-        validar(nombre, mail, contraseña);
-
+    public Usuario modificar(String id, String nombre, String mail, String contrasenia, String contrasenia1, MultipartFile archivo) throws Exception {
+        
+        validar(nombre, mail, contrasenia, contrasenia1);
+        
         Usuario usuario = buscarPorId(id);
         usuario.setNombre(nombre);
         usuario.setMail(mail);
-        usuario.setContraseña(contraseña);
+        usuario.setContrasenia(contrasenia);
         String idImagen = null;
         if (usuario.getImagen() != null) {
             idImagen = usuario.getImagen().getId();
         }
         Imagen imagen = imagenServicio.modificar(idImagen, archivo);
         usuario.setImagen(imagen);
-
+        
         return usuarioRepositorio.save(usuario);
     }
-
+    
     @Transactional(rollbackFor = Exception.class)
     public void Eliminar(String id) throws Exception {
-
+        
         Usuario usuario = buscarPorId(id);
-
+        
         usuarioRepositorio.delete(usuario);
     }
-
+    
     @Transactional(readOnly = true)
     public List<Usuario> listar() {
         return usuarioRepositorio.findAll();
     }
-
-    private void validar(String nombre, String mail, String contraseña) throws Exception {
-
+    
+    private void validar(String nombre, String mail, String contrasenia, String contrasenia1) throws Exception {
+        
         if (nombre == null || nombre.isEmpty()) {
             throw new Exception("Nombre vacío");
         }
-
+        
         if (mail == null || mail.isEmpty()) {
             throw new Exception("E-mail vacío");
         }
-
-        if (contraseña == null || contraseña.isEmpty()) {
+        
+        if (contrasenia == null || contrasenia.isEmpty()) {
             throw new Exception("Contraseña vacío");
         }
+        
+        if (contrasenia1 == null || contrasenia1.isEmpty()) {
+            throw new Exception("Contraseña vacío");
+        }
+        
+        if (!contrasenia.equals(contrasenia1)) {
+            throw new Exception("No coinciden las contraseñas");
+        }
     }
-
+    
 }
